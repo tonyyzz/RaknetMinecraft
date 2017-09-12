f7df5803cafad24a68685d56f10610af075b624f
yzz <1277745281@qq.com>
2017-09-12 18:23:01 +0800

    -

--- Minecraft/UdpHouseTest/package/PlayerLoginRetPacket.cs
+++ Minecraft/UdpHouseTest/package/PlayerLoginRetPacket.cs
@@ -7,33 +7,34 @@ using System.Threading.Tasks;
 
 namespace UdpHouseTest.package
 {
-	/// <summary>
-	/// 登录返回接口
-	/// </summary>
-	class PlayerLoginRetPacket : Package
-	{
-		public PlayerLoginRetPacket() { }
-		public PlayerLoginRetPacket(byte[] buffer, int msgLen, MainCommand mainid, SecondCommand secondid) : base(buffer, msgLen, mainid, secondid) { }
-		public override Package Clone()
-		{
-			Package pack = new PlayerLoginRetPacket(null, 0,
-				MainCommand.MC_ACCOUNT, SecondCommand.SC_ACCOUNT_login_ret);
-			return pack;
-		}
-		public override void ReadPackage() { }
-		public override void WritePackage() { }
-		public override void Excute()
-		{
-			string name = ReadString(); //昵称
-			int sex = ReadInt(); //性别
-			int money = ReadInt(); //金币
+    /// <summary>
+    /// 登录返回接口
+    /// </summary>
+    class PlayerLoginRetPacket : Package
+    {
+        public PlayerLoginRetPacket() { }
+        public PlayerLoginRetPacket(byte[] buffer, int msgLen, MainCommand mainid, SecondCommand secondid) : base(buffer, msgLen, mainid, secondid) { }
+        public override Package Clone()
+        {
+            Package pack = new PlayerLoginRetPacket(null, 0,
+                MainCommand.MC_ACCOUNT, SecondCommand.SC_ACCOUNT_login_ret);
+            return pack;
+        }
+        public override void ReadPackage() { }
+        public override void WritePackage() { }
+        public override void Excute()
+        {
+            int playerId = ReadInt();
+            string name = ReadString(); //昵称
+            int sex = ReadInt(); //性别
+            int money = ReadInt(); //金币
 
-			PlayerMng.player.Name = name;
-			PlayerMng.player.Sex = sex;
-			PlayerMng.player.Money = money;
+            PlayerMng.player.Name = name;
+            PlayerMng.player.Sex = sex;
+            PlayerMng.player.Money = money;
 
 
-			CommonForm.Obj.loginForm.BeginInvoke(new Action(CommonForm.Obj.loginForm.LoginSuccess));
-		}
-	}
+            CommonForm.Obj.loginForm.BeginInvoke(new Action(CommonForm.Obj.loginForm.LoginSuccess));
+        }
+    }
 }
---
+++ Minecraft/UdpHouseTest/udpRaknet/UdpRaknetPeerManager.cs
@@ -0,0 +1,130 @@
+﻿using RakNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UdpHouseTest
+{
+    public class UdpRaknetPeerManager
+    {
+        private static UdpRaknetPeerManager Instance = null;
+
+        private RakPeerInterface rakPeer = null;
+        private readonly static object obj = new object();
+        private UdpRaknetPeerManager() { }
+        public static UdpRaknetPeerManager GetInstance()
+        {
+            if (Instance == null)
+            {
+                lock (obj)
+                {
+                    if (Instance == null)
+                    {
+                        Instance = new UdpRaknetPeerManager();
+                        Instance.rakPeer = RakPeerInterface.GetInstance();
+                        NatPunchthroughServer natPunchthroughServer = new NatPunchthroughServer();
+                        Instance.rakPeer.AttachPlugin(natPunchthroughServer);
+                    }
+                }
+            }
+            return Instance;
+        }
+
+        /// <summary>
+        /// 作为服务器启动
+        /// </summary>
+        /// <param name="maxConnCount"></param>
+        /// <returns></returns>
+        public bool Start(ushort maxConnCount = 6)
+        {
+            rakPeer.SetMaximumIncomingConnections(maxConnCount);
+            var result = rakPeer.Startup(1, new SocketDescriptor(), 1);
+            if (result == StartupResult.RAKNET_ALREADY_STARTED)
+            {
+                ReceiveThreadStart();
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 作为客户端链接服务器，同时也可作为服务器被连接
+        /// </summary>
+        public void Connect(ushort maxConnCount = 6)
+        {
+
+        }
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        public void Send()
+        {
+
+        }
+
+        /// <summary>
+        /// 接收消息线程池
+        /// </summary>
+        private void ReceiveThreadStart()
+        {
+            while (true)
+            {
+                using (Packet testPacket = rakPeer.Receive())
+                {
+                    if (testPacket == null || testPacket.data.Count() <= 0)
+                    {
+                        Thread.Sleep(1);
+                        continue;
+                    }
+
+                    DefaultMessageIDTypes defaultMessageIDType = (DefaultMessageIDTypes)testPacket.data[0];
+                    Console.WriteLine(string.Format(@"{0} - {1}", testPacket.data[0], defaultMessageIDType.ToString()));
+
+                    switch (defaultMessageIDType)
+                    {
+                        case DefaultMessageIDTypes.ID_NEW_INCOMING_CONNECTION: //有客户端链接
+                            {
+
+                            }
+                            break;
+                        case DefaultMessageIDTypes.ID_USER_PACKET_ENUM: //接收消息
+                            {
+                                
+                            }
+                            break;
+                        case DefaultMessageIDTypes.ID_DISCONNECTION_NOTIFICATION: //有客户端断开
+                            {
+
+                            }
+                            break;
+                        default: //操作失败
+                            {
+
+                            }
+                            break;
+                    }
+                    //rakPeer.DeallocatePacket(testPacket);
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        /// <param name="beforeAction"></param>
+        public void Stop(Action beforeAction = null)
+        {
+            beforeAction?.Invoke();
+            Thread.Sleep(5);
+            rakPeer.Shutdown(300);
+            RakPeerInterface.DestroyInstance(rakPeer);
+            rakPeer = null;
+            Instance = null;
+        }
+    }
+}
